13680fad64af4ca325057472ad9dd8d1c634a9af
shreym3 <shreym3@gmail.com>
2020-09-12 12:31:37 +0530

    Resposiveness added!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const responsive =  {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
 $(document).ready(function(){
 
     $nav = $('.nav');
@@ -15,7 +30,8 @@ $(document).ready(function(){
         autoplayTimeout:3000,
         dots:false,
         nav:true,
-        navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
     });
     //Click to Scroll to top
     $('.move-up span').click(function(){
